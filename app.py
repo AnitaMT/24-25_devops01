883ca296a278a35eda1eb26974a52deaae5504bd
AnitaMT <amortro399@g.educaand.es>
2025-01-28 13:50:04 +0100

    modificado para probar testing

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 
 def suma(a: int, b: int) -> int:
     """ docstring function
-    # debe dar errores en linter
+    # debe dar errores en linter modificado
     """
     # x = "no se usa"
     return a + b
